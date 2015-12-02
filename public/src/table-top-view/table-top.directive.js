beac3239dfd3b4699db14b29cc4e9a8beef4766d
Chris Stevens <munkeyxis@gmail.com>
2015-12-01 20:43:17 -0500

    comment out mock party members

--- public/src/table-top-view/table-top.directive.js
+++ public/src/table-top-view/table-top.directive.js
@@ -32,7 +32,9 @@
     function TableTopController($log, socket) {
         const vm = this;
 
-        vm.partyCharacters = [{name:'Player 1', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 2', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 3', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 4', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}];
+        // vm.partyCharacters = [{name:'Player 1', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 2', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 3', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 4', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}];
+
+        vm.partyCharacters = [];
 
         socket.on('addCharacter', data => {
             $log.info('adding character to table', data.name);
