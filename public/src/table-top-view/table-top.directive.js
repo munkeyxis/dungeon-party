9ac6452e289c177e4288a2b7a75a3a19b11e888b
Chris Stevens <munkeyxis@gmail.com>
2015-12-05 00:25:32 -0500

    apply styles based on class

--- public/src/table-top-view/table-top.directive.js
+++ public/src/table-top-view/table-top.directive.js
@@ -9,6 +9,7 @@
         <player-party>
             <character-card 
                 ng-repeat="character in tableTop.partyCharacters"
+                class="{{character.class | lowercase}}"
                 character-data="character"></character-card>
         </player-party>
         <roll-display></roll-display>
@@ -32,9 +33,9 @@
     function TableTopController($log, socket) {
         const vm = this;
 
-        // vm.partyCharacters = [{name:'Player 1', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 2', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 3', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 4', race: 'poop', class: 'peepee', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}];
+        vm.partyCharacters = [{name:'Player 1', race: 'poop', class: 'Fighter', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 2', race: 'poop', class: 'Ranger', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 3', race: 'poop', class: 'Paladin', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 4', race: 'poop', class: 'Warlock', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}];
 
-        vm.partyCharacters = [];
+        // vm.partyCharacters = [];
 
         socket.on('addCharacter', data => {
             $log.info('adding character to table', data.name);
