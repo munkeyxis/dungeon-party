54c53e34e7346bb178c86e12427f7dc045bf997d
Chris Stevens <chris.stevens@surveysampling.com>
2015-12-06 20:21:19 -0500

    disable mock data

--- public/src/table-top-view/table-top.directive.js
+++ public/src/table-top-view/table-top.directive.js
@@ -7,7 +7,7 @@
 
     var template = `
         <player-party>
-            <character-card 
+            <character-card
                 ng-repeat="character in tableTop.partyCharacters"
                 class="{{character.class | lowercase}}"
                 character-data="character"></character-card>
@@ -33,9 +33,9 @@
     function TableTopController($log, socket) {
         const vm = this;
 
-        vm.partyCharacters = [{name:'Player 1', race: 'poop', class: 'Fighter', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 2', race: 'poop', class: 'Ranger', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 3', race: 'poop', class: 'Paladin', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 4', race: 'poop', class: 'Warlock', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}];
+        // vm.partyCharacters = [{name:'Player 1', race: 'poop', class: 'Fighter', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 2', race: 'poop', class: 'Ranger', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 3', race: 'poop', class: 'Paladin', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}, {name:'Player 4', race: 'poop', class: 'Warlock', currentHitPoints: 10, maxHitPoints: 10, armorClass: 10}];
 
-        // vm.partyCharacters = [];
+        vm.partyCharacters = [];
 
         socket.on('addCharacter', data => {
             $log.info('adding character to table', data.name);
